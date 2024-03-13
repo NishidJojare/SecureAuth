2b8e151f75cc4059a1463dbfcd850a7606710b74
Nishid Jojare <developer.nishid@gmail.com>
2024-03-13 10:07:16 +0530

    Secure Authentication System

---
+++ learningauth/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for learningauth project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learningauth.settings')
+
+application = get_asgi_application()
